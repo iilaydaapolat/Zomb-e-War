1297f730d193f5b46e07a4a2527dc91d27677aaa
iilaydaapolat <82779853+iilaydaapolat@users.noreply.github.com>
2021-05-05 00:42:02 +0300

    Zombie War
    Player ve zombiler arasında geçen bir savaş oyunudur.
    Farklı bir çok opsiyonlar içerir

---
+++ Zombie War/Assets/L_FG_Assets_Pack_Lite/Demo/Assets/Scripts/Main Menu/Mainmenu.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Mainmenu : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public void PlayGame()
+	{
+		SceneManager.LoadScene("TestScene");
+	}
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
+
